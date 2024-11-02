3bafb3516273ade69dae93a19a0dba180c0f4f3c
Yonny <yohanssamuel2014@gmail.com>
2024-11-02 20:20:18 +0300

    initial commit

---
+++ src/main/java/com/yohans/userbankinfo/UserbankinfoApplication.java
@@ -0,0 +1,13 @@
+package com.yohans.userbankinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserbankinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserbankinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yohans/userbankinfo/UserbankinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yohans.userbankinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserbankinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
